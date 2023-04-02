d0755edb240e03b2a5b82936122cad513c6e7537
Huanyue17 <h.liao@wustl.edu>
2023-04-02 14:18:38 -0500

    baseline

--- view_baseline.js
+++ view_baseline.js
@@ -513,6 +513,7 @@ let perm_entry_dialog = $('#permentry').dialog({
             text: "OK",
             id: "permission-entry-ok-button",
             click: function() {
+                let filepath = $('#advdialog').attr('filepath');
                 open_advanced_dialog($('#advdialog').attr('filepath') )// redo advanced dialog (recalc permissions)
                 perm_dialog.attr('filepath', filepath) // reload contents of permissions dialog
                 $( this ).dialog( "close" );
